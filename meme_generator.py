aaadbef4fb5f57c4fd22d6551fe59552f53f93c1
Jeko <57463439+Jeko-Mus@users.noreply.github.com>
2022-04-18 15:23:09 +0200

    Add files via upload

---
+++ meme_generator.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# import image
+meme = cv2.imread('img/dive.png')
+
+# convert from BGR to RGB
+rgb_meme = cv2.cvtColor(meme, cv2.COLOR_BGR2RGB)
+plt.imshow(rgb_meme)
+
+# input text and text borders to create meme
+meme_copy = rgb_meme.copy()
+plt.figure(figsize = (10,8))
+cv2.rectangle(meme_copy, (250,140), (580,100), (0,0,255), 1)
+cv2.rectangle(meme_copy, (250,85), (530,20), (255,0,255), 1)
+cv2.putText(meme_copy, 'Olympics', (250, 70), cv2.FONT_ITALIC, 2, (0,0,0))
+cv2.putText(meme_copy, 'swim champion, first place', (250, 115), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0))
+cv2.putText(meme_copy, '... i suppose dreams do come true!', (270, 130), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0))
+plt.imshow(meme_copy)
